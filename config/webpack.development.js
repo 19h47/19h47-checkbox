0cd41def00de9e6c2059ab58f0e63d8ff7f26047
19h47 <jeremylevron@19h47.fr>
2019-04-02 00:33:21 +0200

    :tada: Initial commit

---
+++ config/webpack.development.js
@@ -0,0 +1,20 @@
+/**
+ *
+ * @file   webpack.development.js
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ */
+
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(
+    common,
+    {
+        output: {
+            filename: '[name].js'
+        },
+        mode: 'development',
+        devtool: 'source-map',
+        watch: true,
+    },
+);
---
+++ src/index.js
@@ -0,0 +1,118 @@
+/**
+ * Class Checkbox
+ *
+ * 	<div class="js-checkbox" data-condition-class="">
+ * 		<button type="button"></button>
+ * 		<div style="display: none;">
+ *			<input id="option_1" name="option_1" value="false" type="checkbox">
+ *		</div>
+ *	</div>
+ * `
+ * @param obj element DOM element.
+ * @author Jérémy Levron <jeremylevron@19h47.fr> (http://19h47.fr)
+ */
+export default class Checkbox {
+	constructor(element) {
+		this.$element = element;
+	}
+
+	init() {
+		if (null === this.$element) return false;
+
+		this.$button = this.$element.querySelector('button');
+		this.$checkbox = this.$element.querySelector('input');
+
+		this.isActive = this.$element.classList.contains('is-active');
+
+		// Condition
+		const conditionClass = this.$element.getAttribute('data-condition-class');
+		this.conditionalEls = document.querySelectorAll(`.${conditionClass}`) || [];
+
+		this.initEvents();
+
+		return true;
+	}
+
+
+	/**
+	 * Trigger event
+	 *
+	 * @param  obj element
+	 * @param  str name Event name
+	 * @return
+	 */
+	static triggerEvent(element, name) {
+		const event = new Event(name);
+
+		return element.dispatchEvent(event);
+	}
+
+	initEvents() {
+		this.$element.addEventListener('click', () => {
+			Checkbox.triggerEvent(this.$checkbox, 'change');
+			this.toggle();
+		});
+
+		if (this.$checkbox.checked) {
+			this.activate();
+		}
+	}
+
+
+	/**
+	 * Checkbox.toggle
+	 */
+	toggle() {
+		if (this.isActive) return this.deactivate();
+
+		return this.activate();
+	}
+
+
+	/**
+	 * Checkbox.activate
+	 *
+	 * @return	bool
+	 */
+	activate() {
+		if (this.isActive) return false;
+
+		this.isActive = true;
+		this.$element.classList.add('is-active');
+		this.$button.classList.add('is-selected');
+
+		// Condition.
+		for (let i = 0; i < this.conditionalEls.length; i += 1) {
+			this.conditionalEls[i].classList.remove('is-off');
+		}
+
+		this.$checkbox.value = 'true';
+		this.$checkbox.setAttribute('checked', true);
+
+		return true;
+	}
+
+
+	/**
+	 * Checkbox.deactivate
+	 *
+	 * @return	bool
+	 */
+	deactivate() {
+		if (!this.isActive) return false;
+
+		this.isActive = false;
+		this.$element.classList.remove('is-active');
+		this.$button.classList.remove('is-selected');
+
+		// Condition.
+		for (let i = 0; i < this.conditionalEls.length; i += 1) {
+			this.conditionalEls[i].classList.add('is-off');
+		}
+
+		this.$checkbox.value = 'false';
+		this.$checkbox.removeAttribute('checked');
+
+		return true;
+	}
+}
