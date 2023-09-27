60c05780e5db19bba162f76817e88d11ebcbfbff
19h47 <jeremylevron@19h47.fr>
2023-09-27 12:00:21 +0200

    :arrow_up: Upgrade dependencies

--- .babelrc.js
+++ .babelrc.js
@@ -2,11 +2,12 @@ const presets = [
 	[
 		'@babel/preset-env',
 		{
-			targets: '>0.25%',
+			useBuiltIns: 'entry',
+			corejs: '3.32.4',
 		},
 	],
 ];
 
-const plugins = ['@babel/plugin-transform-runtime', '@babel/plugin-transform-class-properties'];
+const plugins = ['@babel/plugin-transform-runtime'];
 
 module.exports = { presets, plugins };
